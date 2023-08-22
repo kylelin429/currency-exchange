d1aac2762a84db0d2f90c10ed2c77a04560bb579
kyle <kylecw.lin@ogilvy.com>
2023-08-22 11:54:10 +0800

    refactor

--- tests/Feature/ConvertTest.php
+++ tests/Feature/ConvertTest.php
@@ -9,19 +9,31 @@ use App\Http\Services\Currency\Currency;
 
 class ConvertTest extends TestCase
 {
-    public function test_conversion()
+    public function successProvider()
     {
-        $currency = new Currency('USD', '$1,525');
+        return [
+            ['$1,525', 'USD', 'JPY', 111.801, '170496.52500000000000'],
+            ['$12,479', 'USD', 'USD', 1, '12479.00000000000000'],
+            ['$123,091', 'USD', 'TWD', 30.444, '3747382.40400000000000'],
+        ];
+    }
+
+    /**
+     * @dataProvider successProvider
+     */
+    public function test_conversion($sourceAmt, $sourceCode, $targetCode, $ratio, $result)
+    {
+        $currency = new Currency($sourceCode, $sourceAmt);
         $converter = \Mockery::mock('\App\Http\Services\Currency\CurrencyConverter')->makePartial();
         $converter->shouldReceive('getConversionRatio')
             ->once()
-            ->with($currency->getCode(), 'JPY')
-            ->andReturn(111.801);
+            ->with($currency->getCode(), $targetCode)
+            ->andReturn($ratio);
 
-        $resultCurrency = $converter->convert($currency, 'JPY');
+        $resultCurrency = $converter->convert($currency, $targetCode);
 
         $this->assertInstanceOf(Currency::class, $resultCurrency);
-        $this->assertEquals('170496.52500000000000', $resultCurrency->getAmount());
-        $this->assertEquals('JPY', $resultCurrency->getCode());
+        $this->assertEquals($result, $resultCurrency->getAmount());
+        $this->assertEquals($targetCode, $resultCurrency->getCode());
     }
 }
